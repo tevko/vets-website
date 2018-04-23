3fffcf7773f8e42459d261f2a361dd0d5e4f09a9
Dittrich, Jason F <jason.dittrich@va.gov>
2018-04-23 13:58:22 -0400

    Initial form generation and link to schema

--- src/js/direct-deposit/config/form.js
+++ src/js/direct-deposit/config/form.js
@@ -1,11 +1,49 @@
-// import fullSchema from 'vets-json-schema/dist/24-0296-schema.json';
+import _ from 'lodash/fp';
 
+// Direct Deposit Enrollment Schema
+import fullSchema from 'vets-json-schema/dist/24-0296-schema.json';
+
+// Default Start/End 
 import IntroductionPage from '../containers/IntroductionPage';
 import ConfirmationPage from '../containers/ConfirmationPage';
 
-// const { } = fullSchema.properties;
+// UI components
+import fullNameUI from '../../common/schemaform/definitions/fullName';
+import ssnUI from '../../common/schemaform/definitions/ssn';
+import dateUI from '../../common/schemaform/definitions/date';
+import bankAccountUI from '../../common/schemaform/definitions/bankAccount';
+import phoneUI from '../../common/schemaform/definitions/phone';
+import * as address from '../../common/schemaform/definitions/address';
 
-// const { } = fullSchema.definitions;
+// properties for fields pulled from schema
+const {
+  institutionName,
+  beneficiaryAddressIsNew,
+  benefitType,
+  veteranFullName,
+  veteranSSN,
+  veteranVAFileNumber,
+  veteranDOB,
+  beneficiaryFullName,
+  beneficiarySSN,
+  beneficiaryVAFileNumber,
+  beneficiaryAddress,
+  institutionAccount,
+  institutionAddress,
+  institutionPhone,
+  payeePhone
+} = fullSchema.properties;
+
+// definitions pulled from schema
+const {
+  fullName,
+  ssn,
+  vaFileNumber,
+  date,
+  bankAccount,
+  phone
+} = fullSchema.definitions;
+
 
 const formConfig = {
   urlPrefix: '/',
@@ -22,19 +60,145 @@ const formConfig = {
   },
   title: 'Direct Deposit Enrollment',
   defaultDefinitions: {
+    fullName,
+    bankAccount,
+    phone,
+    date,
+    ssn,
+    vaFileNumber,
+    address
   },
   chapters: {
-    chapter1: {
-      title: 'Chapter 1',
+    veteran: {
+      title: "Veteran's Identification Information",
       pages: {
-        page1: {
-          path: 'first-page',
-          title: 'First Page',
+        veteranInfo: {
+          path: 'veteran-info',
+          title: '',
           uiSchema: {
+            veteranFullName: fullNameUI,
+            veteranSSN: _.merge(ssnUI, {
+              'ui:title': 'Social Security Number',
+              'ui:required': (formData) => !formData.veteranVAFileNumber,
+              'ui:errorMessages': {
+                required: 'Either SSN or VA File Number is required'
+              }
+            }),
+            veteranVAFileNumber: {
+              'ui:title': 'VA File Number',
+              'ui:required': (formData) => !formData.veteranSSN,
+              'ui:errorMessages': {
+                required: 'Either SSN or VA File Number is required'
+              }
+            },
+            veteranDOB: dateUI('Date of Birth')
           },
           schema: {
             type: 'object',
+            required: [
+              'veteranFullName',
+              'veteranDOB'
+            ],
             properties: {
+              veteranFullName,
+              veteranSSN,
+              veteranVAFileNumber,
+              veteranDOB
+            }
+          }
+        }
+      }
+    },
+    beneficiary: {
+      title: "Beneficiary's Identification Information",
+      pages: {
+        beneficiaryInfo: {
+          path: 'beneficiary-info',
+          title: '',
+          uiSchema: {
+            beneficiaryFullName: fullNameUI,
+            beneficiarySSN: _.merge(ssnUI, {
+              'ui:title': 'Social Security Number',
+              'ui:required': (formData) => !formData.beneficiaryVAFileNumber,
+              'ui:errorMessages': {
+                required: 'Either SSN or VA File Number is required'
+              }
+            }),
+            beneficiaryVAFileNumber: {
+              'ui:title': 'VA File Number',
+              'ui:required': (formData) => !formData.beneficiarySSN,
+              'ui:errorMessages': {
+                required: 'Either SSN or VA File Number is required'
+              }
+            },
+            benefitType: {
+              'ui:title': 'Type of Benefit'
+            },
+            beneficiaryAddress: address.uiSchema('Address of Person Receiving Payment'),
+            beneficiaryAddressIsNew: {
+              'ui:title': 'Check if the above address is new'
+            }
+          },
+          schema: {
+            type: 'object',
+            required: [
+              'beneficiaryAddress',
+              'beneficiaryFullName',
+              'benefitType'
+            ],
+            properties: {
+              beneficiaryFullName,
+              beneficiarySSN,
+              beneficiaryVAFileNumber,
+              benefitType,
+              beneficiaryAddress: address.schema(fullSchema, true),
+              beneficiaryAddressIsNew
+            }
+          }
+        }
+      }
+    },
+    financial: {
+      title: 'Financial Institution Information',
+      pages: {
+        financialInfo: {
+          path: 'financial-info',
+          title: '',
+          uiSchema: {
+            institutionAccount: _.merge(bankAccountUI, { 'ui:title': 'Bank Account Information' }),
+            institutionName: { 'ui:title': 'Name of Financial Institution' },
+            institutionAddress: address.uiSchema('Address of Financial Institution'),
+            institutionPhone: _.merge(phoneUI, { 'ui:title': 'Phone Number of Financial Institution' })
+          },
+          schema: {
+            type: 'object',
+            required: [
+              'institutionAccount',
+              'institutionName',
+              'institutionPhone'
+            ],
+            properties: {
+              institutionAccount,
+              institutionName,
+              institutionAddress: address.schema(fullSchema, true),
+              institutionPhone
+            }
+          }
+        },
+        payee: {
+          path: 'payee-contact',
+          uiSchema: {
+            payeePhone: {
+              'ui:title': 'Payee Phone Number'
+            }
+          },
+          schema: {
+            type: 'object',
+            required: [
+              'payeePhone'
+            ],
+            properties: {
+              payeePhone
             }
           }
         }
