eff5604a68dabe1243dad5801a2525399c2ef8ae
R Matthews <remimatthews@brandeis.edu>
2018-06-25 15:20:30 -0400

    updates to form.js

--- src/applications/gi-bill-complaint-tool/config/form.js
+++ src/applications/gi-bill-complaint-tool/config/form.js
@@ -15,3 +15,95 @@ import FormFooter from '../../../platform/forms/components/FormFooter';
 
 import IntroductionPage from '../containers/IntroductionPage';
 import ConfirmationPage from '../containers/ConfirmationPage';
+
+const formConfig = {
+  title: 'GI Bill Complaint Tool',
+  subTitle: 'A subtitle will potentially go here',
+  formId: '',
+  urlPrefix: '/',
+  trackingPrefix: 'form-',
+  transformForSubmit: '',
+  submitUrl: '',
+  introduction: IntroductionPage,
+  confirmation: '',
+  defaultDefinitions: {
+    //fullName
+    test
+  },
+  chapters: {
+    firstSection: {
+      title: 'Filing Information',
+      pages: {
+        firstPage: {
+          path: 'first-section/first-page',
+          title: 'First Page',
+          uiSchema: {
+            test: testUI
+          },
+          schema: {
+            type: 'object',
+            properties: {
+              test
+            }
+          }
+        }
+      }
+    },
+    secondSection: {
+      title: 'School Information',
+      pages: {
+        thirdPage: {
+          path: 'second-section/first-page',
+          title: 'School Information',
+          uiSchema: {
+            test: testUI
+          },
+          schema: {
+            type: 'object',
+            properties: {
+              test
+            }
+          }
+        }
+      }
+    },
+    thirdSection: {
+      title: 'Benefits Used',
+      pages: {
+        thirdPage: {
+          path: 'third-section/first-page',
+          title: 'Benefits Used',
+          uiSchema: {
+            test: testUI
+          },
+          schema: {
+            type: 'object',
+            properties: {
+              test
+            }
+          }
+        }
+      }
+    },
+    fourthSection: {
+      title: 'Issue Description',
+      pages: {
+        firstPage: {
+          path: 'fourth-section/first-page',
+          title: 'Issue Description',
+          uiSchema: {
+            test: testUI
+          },
+          schema: {
+            type: 'object',
+            properties: {
+              test
+            }
+          }
+        }
+      }
+    }
+  }
+};
+
+export default formConfig;
