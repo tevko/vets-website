4127bdbf4cea1a3670b478651d255480f92d1f3a
R Matthews <remimatthews@brandeis.edu>
2018-07-16 16:07:32 -0400

    schema changes and name change

--- src/applications/edu-benefits/complaint-tool/config/form.js
+++ src/applications/edu-benefits/complaint-tool/config/form.js
@@ -275,7 +275,7 @@ const formConfig = {
     notFound: 'Please start over to apply for declaration of status of dependents.',
     noAuth: 'Please sign in again to continue your application for declaration of status of dependents.'
   },
-  title: 'GI Bill Complaint Tool',
+  title: 'GI Bill School Complaint Tool',
   defaultDefinitions: {
   },
   chapters: {
@@ -290,29 +290,47 @@ const formConfig = {
               'ui:title': 'I am filing on behalf of:',
               'ui:widget': 'radio',
               'ui:options': {
-                expandUnderClassNames: 'schemaform-expandUnder'
-              }
+                expandUnderClassNames: 'schemaform-expandUnder',
+                labels: {
+                  1: 'Myself',
+                  2: 'Someone else',
+                  3: 'I want to file my complaint anonymously'
+                },
+                nestedContent: {
+                  1: 'Only your name will be shared with the school.',
+                  2: 'Only your name will be shared with the school.',
+                  3: 'None of your personal information will be shared outside of the VA.'
+                }
+              },
             },
-            personalInfo: {
+            myselfInfo: {
               'ui:options': {
-                hideIf: formData => formData.filing === 'Anonymous',
+                hideIf: formData => formData.filing !== 1,
                 expandUnder: 'filing'
               },
 
               first: {
-                'ui:title': 'First Name',
-                'ui:required': formData => formData.filing !== 'Anonymous'
+                'ui:title': 'Your first name',
+                'ui:required': formData => formData.filing !== 3
               },
               middle: {
-                'ui:title': 'Middle Name',
-                'ui:required': formData => formData.filing !== 'Anonymous'
+                'ui:title': 'Your middle name',
               },
               last: {
-                'ui:title': 'Last Name',
-                'ui:required': formData => formData.filing !== 'Anonymous'
+                'ui:title': 'Your last name',
+                'ui:required': formData => formData.filing !== 3
               },
               suffix: {
-                'ui:title': 'Suffix'
+                'ui:title': 'Suffix',
+                'ui:options': {
+                  labels: {
+                    1: 'Jr.',
+                    2: 'Sr.',
+                    3: 'II',
+                    4: 'III',
+                    5: 'IV'
+                  }
+                }
               },
               dob: {
                 'ui:title': 'Date of Birth',
@@ -321,30 +339,85 @@ const formConfig = {
                   inline: true
                 }
               },
-              serviceAffiliation: {
-                'ui:title': 'Am I a veteran?',
+            },
+            serviceAffiliation: {
+              'ui:title': 'Are you a veteran?',
+              'ui:widget': 'radio',
+              'ui:required': formData => formData.filing === 1,
+              'ui:options': {
+                labels: {
+                  1: 'Yes',
+                  2: 'No'
+                },
+                hideIf: formData => formData.filing !== 1,
+                expandUnderClassNames: 'schemaform-expandUnder'
+              }
+            },
+            someoneInfo: {
+              'ui:options': {
+                hideIf: formData => formData.filing !== 2,
+                expandUnder: 'filing'
+              },
+
+              first: {
+                'ui:title': 'First Name',
+                'ui:required': formData => formData.filing !== 3
+              },
+              middle: {
+                'ui:title': 'Middle Name',
+              },
+              last: {
+                'ui:title': 'Last Name',
+                'ui:required': formData => formData.filing !== 3
+              },
+              suffix: {
+                'ui:title': 'Suffix',
                 'ui:options': {
-                  expandUnderClassNames: 'schemaform-expandUnder'
+                  labels: {
+                    1: 'Jr.',
+                    2: 'Sr.',
+                    3: 'II',
+                    4: 'III',
+                    5: 'IV'
+                  }
+                }
+              },
+              dob: {
+                'ui:title': 'Date of Birth',
+                'ui:widget': 'date',
+                'ui:options': {
+                  inline: true
                 }
               }
             },
             militaryHistory: {
               'ui:options': {
-                hideIf: formData => formData.filing === 'Anonymous',
-                expandUnder: 'serviceAffiliation'
+                hideIf: formData => formData.serviceAffiliation !== 1,
+                expandUnder: 'serviceAffiliation',
               },
               branch: {
                 'ui:title': 'Branch',
+                'ui:required': formData => formData.serviceAffiliation === 1,
+                'ui:options': {
+                  labels: {
+                    1: 'Army',
+                    2: 'Navy',
+                    3: 'Marines',
+                    4: 'Air Force',
+                    5: 'Coast Guard',
+                    6: 'NOAA/PHS'
+                  }
+                }
               },
               eod: {
-                'ui:title': 'Entered on Duty',
+                'ui:title': 'Service start date',
                 'ui:widget': 'date',
                 'ui:options': {
                   inline: true,
                 }
               },
               rad: {
-                'ui:title': 'Release from Active Duty',
+                'ui:title': 'Service end date',
                 'ui:widget': 'date',
                 'ui:options': {
                   inline: true,
@@ -361,12 +434,12 @@ const formConfig = {
               filing: {
                 type: 'string',
                 enum: [
-                  'Myself',
-                  'Someone Else',
-                  'Anonymous'
+                  1,
+                  2,
+                  3
                 ]
               },
-              personalInfo: {
+              myselfInfo: {
                 type: 'object',
                 properties: {
                   first: {
@@ -387,23 +460,56 @@ const formConfig = {
                   suffix: {
                     type: 'string',
                     'enum': [
-                      'Jr.',
-                      'Sr.',
-                      'II',
-                      'III',
-                      'IV'
+                      1,
+                      2,
+                      3,
+                      4,
+                      5
                     ]
                   },
                   dob: {
                     type: 'string'
                   },
-                  serviceAffiliation: {
+                }
+              },
+              serviceAffiliation: {
+                type: 'string',
+                enum: [
+                  1,
+                  2
+                ]
+              },
+              someoneInfo: {
+                type: 'object',
+                properties: {
+                  first: {
                     type: 'string',
-                    enum: [
-                      'Yes',
-                      'No'
+                    pattern: '^.*\\S.*',
+                    minLength: 1,
+                    maxLength: 30
+                  },
+                  middle: {
+                    type: 'string'
+                  },
+                  last: {
+                    type: 'string',
+                    pattern: '^.*\\S.*',
+                    minLength: 2,
+                    maxLength: 30
+                  },
+                  suffix: {
+                    type: 'string',
+                    'enum': [
+                      1,
+                      2,
+                      3,
+                      4,
+                      5
                     ]
-                  }
+                  },
+                  dob: {
+                    type: 'string'
+                  },
                 }
               },
               militaryHistory: {
@@ -412,9 +518,12 @@ const formConfig = {
                   branch: {
                     type: 'string',
                     enum: [
-                      'Army',
-                      'Navy',
-                      'etc.'
+                      1,
+                      2,
+                      3,
+                      4,
+                      5,
+                      6
                     ]
                   },
                   eod: {
@@ -431,8 +540,8 @@ const formConfig = {
         },
         secondPage: {
           depends: [
-            {filing: 'Myself'},
-            {filing: 'Someone Else'}
+            {filing: 1},
+            {filing: 2}
           ],
           path: 'first-section/second-page',
           title: 'Second Page',
@@ -441,10 +550,10 @@ const formConfig = {
               'ui:title': 'Country'
             },
             streetAddress1: {
-              'ui:title': 'Address Line 1',
+              'ui:title': 'Address line 1',
             },
             streetAddress2: {
-              'ui:title': 'Address Line 2',
+              'ui:title': 'Address line 2',
             },
             city: {
               'ui:title': 'City',
@@ -453,18 +562,22 @@ const formConfig = {
               'ui:title': 'State',
             },
             postalCode: {
-              'ui:title': 'Postal Code',
+              'ui:title': 'Postal code',
             },
             email: {
               'ui:title': 'Email',
               'ui:widget': 'EmailWidget'
             },
+            emailConfirmation: {
+              'ui:title': 'Re-enter email address',
+              'ui:widget': 'EmailWidget'
+            },
             phone: {
-              'ui:title': 'Phone Number',
+              'ui:title': 'Phone number',
               'ui:options': {
                 "inputType": "tel"
               }
-            },
+            }
           },
           schema: {
             type: 'object',
@@ -474,7 +587,8 @@ const formConfig = {
               'city',
               'state',
               'postalCode',
-              'email'
+              'email',
+              'emailConfirmation'
             ],
             properties: {
               country: {
@@ -550,6 +664,9 @@ const formConfig = {
                 maxLength: 10
               },
               email: {
+                type: 'string'
+              },
+              emailConfirmation: {
                 type: 'string'
               },
               phone: {
@@ -661,10 +778,10 @@ const formConfig = {
               'ui:title': 'Country'
             },
             streetAddress1: {
-              'ui:title': 'Address Line 1',
+              'ui:title': 'Address line 1',
             },
             streetAddress2: {
-              'ui:title': 'Address Line 2',
+              'ui:title': 'Address line 2',
             },
             city: {
               'ui:title': 'City',
@@ -673,8 +790,8 @@ const formConfig = {
               'ui:title': 'State',
             },
             postalCode: {
-              'ui:title': 'Postal Code',
-            },
+              'ui:title': 'Postal code',
+            }
           },
           schema: {
             type: 'object',
